6995bb59ad34614772c70eea96d9ef7c1150d6b3
Metastasis <d1mq44@gmail.com>
2017-01-26 15:18:54 +0300

    Add libs dependencies, fonts, icons

--- app/src/main/java/io/github/metastasis/todoproject/MainActivity.java
+++ app/src/main/java/io/github/metastasis/todoproject/MainActivity.java
@@ -1,7 +1,11 @@
 package io.github.metastasis.todoproject;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalligraphyConfig defaultFontConfig = new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/OpenSans-Light.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build();
+        CalligraphyConfig.initDefault(defaultFontConfig);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
